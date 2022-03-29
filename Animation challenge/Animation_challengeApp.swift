026890cc322e21399fcb01899d4e7a75e7ac7d55
Abigail De Micco <abdemicco@gmail.com>
2022-03-29 09:58:31 +0200

    Initial Commit

---
+++ Animation challenge/Animation_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_challengeApp.swift
+//  Animation challenge
+//
+//  Created by Abigail De Micco on 29/03/22.
+//
+
+import SwiftUI
+
+@main
+struct Animation_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animation challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Animation challenge
+//
+//  Created by Abigail De Micco on 29/03/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
