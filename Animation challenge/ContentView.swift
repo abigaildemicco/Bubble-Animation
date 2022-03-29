cdb8959f912a43d5bf3c82bdc533f9d4e11a6f45
Abigail De Micco <abdemicco@gmail.com>
2022-03-29 18:17:22 +0200

    First commit

--- Animation challenge/ContentView.swift
+++ Animation challenge/ContentView.swift
@@ -6,16 +6,53 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ContentView: View {
+    @State var scale : CGFloat = 1
+    @State var audio : AVAudioPlayer!
+    let rouge = Color(red: 0.22, green: 0.05, blue: 0.14)
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            ForEach (1...50, id:\.self) { _ in
+                Circle ()
+                    .foregroundColor(Color (red: .random(in: 0.3...1),
+                                            green: 0.1,
+                                            blue: .random(in: 0.2...1)))
+                
+                    .blendMode(.colorDodge) // The bottom circle is lightened by an amount determined by the top layer
+                    .animation (Animation.spring (dampingFraction: 0.5)
+                                    .repeatForever()
+                                    .speed (.random(in: 0.05...0.4))
+                                    .delay(.random (in: 0...1)), value: scale
+                    )
+                
+                    .scaleEffect(self.scale * .random(in: 0.1...3))
+                    .frame(width: .random(in: 1...100),
+                           height: CGFloat.random (in:20...100),
+                           alignment: .center)
+                    .position(CGPoint(x: .random(in: 0...1112),
+                                      y: .random (in:0...834)))
+            }
+        }
+        .onAppear {
+            
+            let song = NSDataAsset (name: "AmbientMusic")
+            self.audio = try! AVAudioPlayer(data: song!.data, fileTypeHint: "mp3")
+            self.audio.play()
+            self.scale = 1.2 // default circle scale
+        }
+        
+        .drawingGroup(opaque: false, colorMode: .linear)
+        .background(
+            Rectangle()
+                .foregroundColor(rouge))
+        .ignoresSafeArea()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
