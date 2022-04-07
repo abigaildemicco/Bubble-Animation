f78cef17cfaf51458407bc5332411d55736fd349
Abigail De Micco <abdemicco@gmail.com>
2022-04-07 11:07:34 +0200

    Add some comments

--- Animation challenge/ContentView.swift
+++ Animation challenge/ContentView.swift
@@ -11,7 +11,7 @@ import AVKit
 struct ContentView: View {
     @State var scale : CGFloat = 1
     @State var audio : AVAudioPlayer!
-    let rouge = Color(red: 0.22, green: 0.05, blue: 0.14)
+    let rouge = Color(red: 0.23, green: 0.05, blue: 0.14) //custom color
     var body: some View {
         
         ZStack {
@@ -19,14 +19,14 @@ struct ContentView: View {
                 Circle ()
                     .foregroundColor(Color (red: .random(in: 0.3...1),
                                             green: 0.1,
-                                            blue: .random(in: 0.2...1)))
+                                            blue: .random(in: 0.3...1)))
                 
-                    .blendMode(.colorDodge) // The bottom circle is lightened by an amount determined by the top layer
+                    .blendMode(.colorDodge) // The bottom circle is lightened by the top layer
                     .animation (Animation.spring (dampingFraction: 0.5)
                                     .repeatForever()
                                     .speed (.random(in: 0.05...0.4))
                                     .delay(.random (in: 0...1)), value: scale
-                    )
+                    ) // move the circle to the target point and bounce back
                 
                     .scaleEffect(self.scale * .random(in: 0.1...3))
                     .frame(width: .random(in: 1...100),
@@ -39,12 +39,12 @@ struct ContentView: View {
         .onAppear {
             
             let song = NSDataAsset (name: "AmbientMusic")
-            self.audio = try! AVAudioPlayer(data: song!.data, fileTypeHint: "mp3")
+            self.audio = try! AVAudioPlayer(data: song!.data, fileTypeHint: "mp3") //adds the music track
             self.audio.play()
             self.scale = 1.2 // default circle scale
         }
         
-        .drawingGroup(opaque: false, colorMode: .linear)
+        .drawingGroup(opaque: false, colorMode: .linear) // makes the app faster, rendering the contents of the view into an off-screen image before putting it back onto the screen as a single rendered output
         .background(
             Rectangle()
                 .foregroundColor(rouge))
